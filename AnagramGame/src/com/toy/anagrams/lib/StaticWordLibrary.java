8d5f1dffb698a0e7280cd113c9e9b0ca94811328
Dirk- <dirk.froehling@w-hs.de>
2017-05-09 13:44:34 +0200

    Added word

--- AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ AnagramGame/src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -36,6 +36,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
+        "michael jackson",
         "abstraction",
         "ambiguous",
         "arithmetic",
@@ -83,6 +84,7 @@ final class StaticWordLibrary extends WordLibrary {
         "traditional"};
 
     private static final String[] SCRAMBLED_WORD_LIST = {
+        "naja milchsocke",
         "batsartcoin",
         "maibuguos",
         "ratimhteci",
